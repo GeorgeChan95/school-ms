eb4f90734aa6320125cfca53041440e516302d02
George Chan <george_95@126.com>
2020-07-25 14:40:02 +0800

    整合logback并添加项目启动脚本

--- src/main/java/com/george/school/SchoolApplication.java
+++ src/main/java/com/george/school/SchoolApplication.java
@@ -1,5 +1,7 @@
 package com.george.school;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -18,7 +20,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement // 开启事务支持
 public class SchoolApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SchoolApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
+        LOGGER.info("程序启动成功......");
     }
 }
